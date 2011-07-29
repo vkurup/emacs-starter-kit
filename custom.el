3beec924282628b494a3782f7cf6e42fc9c3f262
Vinod Kurup <vinod@kurup.com>
2011-07-28 20:45:40 -0400

    Stop gitignoring custom.el

---
+++ custom.el
@@ -0,0 +1,19 @@
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(android-mode-sdk-dir "~/src/android-sdk-linux_x86")
+ '(inferior-lisp-program "lein repl")
+ '(nxml-slash-auto-complete-flag t)
+ '(org-agenda-files (quote ("~/Dropbox/org/todo.org")))
+ '(org-modules (quote (org-bbdb org-bibtex org-docview org-gnus org-info org-jsinfo org-irc org-mew org-mhe org-rmail org-vm org-wl org-w3m org-velocity)))
+ '(org-capture-templates (quote (("t" "Create Task" checkitem (file+headline "~/Dropbox/org/autofocus.org" "Last") ""))))
+ '(org-refile-targets (quote ((org-agenda-files :level . 1))))
+ '(temporary-file-directory (concat dotfiles-dir "tmp")))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(org-hide ((default (:foreground "grey25")) (nil nil))))
