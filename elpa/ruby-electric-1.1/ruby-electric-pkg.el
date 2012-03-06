35a9b72f642cc8c63892d9dc4484635313f9ad91
Vinod Kurup <vinod@kurup.com>
2012-03-05 21:24:15 -0500

    Upgraded to ESK v2

---
+++ elpa/ruby-electric-1.1/ruby-electric-pkg.el
@@ -0,0 +1,1 @@
+(define-package #("ruby-electric" 0 13 (fontified t face font-lock-comment-face)) "1.1" "electric editing commands for ruby files" (quote nil))
---
+++ elpa/smex-1.1.2/smex-autoloads.el
@@ -0,0 +1,27 @@
+;;; smex-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (smex-initialize) "smex" "smex.el" (20306 31021))
+;;; Generated autoloads from smex.el
+
+(autoload 'smex-initialize "smex" "\
+
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("smex-pkg.el") (20306 31021 698512))
+
+;;;***
+
+(provide 'smex-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; smex-autoloads.el ends here
